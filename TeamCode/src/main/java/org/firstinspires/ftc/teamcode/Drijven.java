fccd706008b93b6d0652f8de874618fc2fc89b0a
giemiel <114511272+giemiel@users.noreply.github.com>
2022-10-06 09:19:27 +0200

    update grijparm
    ik heb de grijparm en de controlls een naam gegeven, we hoeven nu alleen nog maar de namen van de motors in te vullen

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drijven.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drijven.java
@@ -10,7 +10,9 @@ public class Drijven extends OpMode {
     DcMotor motorRightFront;
     DcMotor motorLeftBack;
     DcMotor motorRightBack;
-
+    'armmotor' motorGrabinator;
+    'pionhouder'motorConeHolder;
+    // de dingen tussen hoge kommas moeten nog worden vervangen met de motornamen
     @Override
     public void init() {
         motorLeftBack = hardwareMap.dcMotor.get("linksachter");
@@ -19,6 +21,9 @@ public class Drijven extends OpMode {
         motorRightFront = hardwareMap.dcMotor.get("rechtsvoor");
         motorRightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         motorRightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorGrabinator = hardwareMap.'armmotor'.get("armextender);
+        motorConeHolder = hardwareMap.'pionhouder'.get("mond");
+        // de dingen tussen hoge kommas moeten nog worden vervangen met de motornamen
     }
 
 
@@ -40,7 +45,8 @@ public class Drijven extends OpMode {
         motorLeftBack.setPower(gamepad1.left_stick_y+gamepad1.left_stick_x+gamepad1.right_stick_x*-1);
         motorRightFront.setPower(gamepad1.left_stick_y+gamepad1.left_stick_x+gamepad1.right_stick_x);
         motorLeftFront.setPower(gamepad1.left_stick_y+gamepad1.left_stick_x*-1+gamepad1.right_stick_x*-1);
-      
+        motograbinator.setPower(gamepad2.right_button+gamepad2.left_button*-1);
+        motorconeholder.setPower(gamepad2.A+gamepad2.X*-1);
     }
     
 
